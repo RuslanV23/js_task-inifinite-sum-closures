eaaa78d6cb2fd1d91f93a736cb12025c8eb96587
Sidach Ruslan <flashangel4600@gmail.com>
2026-03-30 15:29:27 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (...args) => {
+    if (args.length === 0) {
+      const returnSum = sum;
+
+      sum = 0;
+
+      return returnSum;
+    }
+
+    sum += args[0];
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
